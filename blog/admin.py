e62d748bc8336321f7587e7b443ebafcc5e69f52
Dauzy <daniel_6287@hotmail.com>
2017-05-29 11:54:39 -0500

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+
+# Para hacer nuestro modelo visible en la página del
+# administrador, tenemos que registrar el modelo con
+admin.site.register(Post)
