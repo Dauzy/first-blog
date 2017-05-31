58f24de53607ededbece11b0766f00264f8fb244
Dauzy <daniel_6287@hotmail.com>
2017-05-31 17:56:13 -0500

    Add Comments to Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 # Register your models here.
 
 # Para hacer nuestro modelo visible en la página del
 # administrador, tenemos que registrar el modelo con
 admin.site.register(Post)
+admin.site.register(Comment)
