3faf4e46fa3717703ad342851e647981ee22211f
Dauzy <daniel_6287@hotmail.com>
2017-05-31 11:34:25 -0500

    Auth

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name="postdetail"),
     url(r'^post/new/$', views.post_new, name='postnew'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='postedit'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
 ]
